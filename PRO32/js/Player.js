7ec44e303b973007b2a25b548febae5789130455
samuel-A-rodriguez-Ojeda <111399989+samuel-A-rodriguez-Ojeda@users.noreply.github.com>
2022-12-12 22:24:46 -0500

    Add files via upload

---
+++ PRO32/js/Player.js
@@ -0,0 +1,51 @@
+class Player {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png");
+
+    this.life1 = "green";
+    this.life2 = "green";
+    this.life3 = "green";
+
+    World.add(world, this.body);
+  }
+
+  life() {
+    push();
+   //escribe el código correcto para mostrar las vidas como tres rectángulos verdes en una línea horizontal.
+   
+   textSize(20);
+   fill("white");
+   text("PLAYER", width - 750, 40);
+
+   fill(this.life1);
+   rect(width - 800, 50, 70, 30);
+   fill(this.life2);
+   rect(width - 730, 50, 70, 30);
+   fill(this.life3);
+   rect(width - 660, 50, 70, 30);
+   
+    pop();
+  }
+
+ 
+
+
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
